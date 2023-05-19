88c22894e25930015d142b1d35121c18eb4f0a5c
shira-chesler <shira.chesler@gmail.com>
2023-05-19 04:45:20 -0400

    starting smartTeam

--- sources/SmartTeam.cpp
+++ sources/SmartTeam.cpp
@@ -2,39 +2,10 @@
 
 using namespace ariel;
 
-SmartTeam::SmartTeam(Character* leader){
 
+SmartTeam::SmartTeam(Character *leader) : Team2(leader) {
 }
-SmartTeam::SmartTeam(SmartTeam &stem){
 
-}
-SmartTeam::SmartTeam(SmartTeam &&stem) noexcept{
+void SmartTeam::attack(ariel::Team *enemys) {
 
-}
-int SmartTeam::stillAlive(){
-    return 0;
-}
-Character* SmartTeam::getLeader(){
-    return this->team.at(static_cast<unsigned long>(0));
-}
-void SmartTeam::attack(SmartTeam *enemys){
-
-}
-void SmartTeam::attack(Team *enemys){
-
-}
-void SmartTeam::add(Character* member){
-
-}
-std::string SmartTeam::print(){
-    return "empy stm";
-}
-SmartTeam::~SmartTeam(){
-
-}
-SmartTeam& SmartTeam::operator=(const SmartTeam& stem){
-    return (*this);
-}
-SmartTeam& SmartTeam::operator=(SmartTeam&& stem) noexcept{
-    return (*this);
 }
--- sources/SmartTeam.hpp
+++ sources/SmartTeam.hpp
@@ -7,28 +7,15 @@
 #include "OldNinja.hpp"
 #include "TrainedNinja.hpp"
 #include "YoungNinja.hpp"
-#include "Team.hpp"
+#include "Team2.hpp"
 
 namespace ariel {
-    class SmartTeam {
-        std::array<Character*, NUM_MEMBERS_OF_A_TEAM> team;
-        bool started_game;
-        int alive_members;
-        int leader_position;
+    class SmartTeam :public Team2{
+
 
         public:
         SmartTeam(Character* leader);
-        SmartTeam(SmartTeam &stem);
-        SmartTeam(SmartTeam &&stem) noexcept;
-        int stillAlive();
-        void attack(SmartTeam *enemys);
-        void attack(Team *enemys);
-        void add(Character* member);
-        Character* getLeader();
-        std::string print();
-        ~SmartTeam();
-        SmartTeam& operator=(const SmartTeam& stem);
-        SmartTeam& operator=(SmartTeam&& stem) noexcept;
+        void attack(Team *enemys) override;
     };
 }
 
